d52e3c311fc09b1ede1ef41637a5728594948be6
wilbrone <wilbroneokoth@gmail.com>
2019-10-28 03:04:45 +0300

    collect form data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,10 @@ import {MatInputModule} from '@angular/material';
     BrowserAnimationsModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.css']
 })
-export class QuotesComponent implements OnInit {
+export class QuotesComponent implements OnInit { 
+
 
   constructor() { }
+  // quotation:Quatation = new Quatation(quotation.quote);
+  quotation: Quatation[] = [];
+
+  // new_quotation = new Quatation();
+
+  onSubmitting(data) {
+    console.log(data);
+
+    this.quotation.push(data);
+    console.log(this.quotation);
+
+    event.preventDefault();
+  }
 
   ngOnInit() {
   }
 
+}
+
+
+export interface Quatation{
+  author: string;
+  name: string;
+  quote: string
 }
